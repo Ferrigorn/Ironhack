998b82e4babf002fa839f05dc75fa83b3063abb5
Ferrigorn <fbabures@gmail.com>
2024-04-24 17:18:50 +0200

    miniEjercicios condicionales terminado

--- Mini-ejercicios_Condicionales/lampara.js
+++ Mini-ejercicios_Condicionales/lampara.js
@@ -1,19 +1,28 @@
 function lampara(){
-    let respuesta = prompt("¿La lampara funciona? (S/N)").toLowerCase
-    if (respuesta == "s"){
-        alert("Qué bien!!")
-    }else {
-        let respuesta2 = prompt("Está enchufada? (s/n)").toLowerCase
-        if(respuesta2 == "s"){
-            let respuesta3 = prompt("La bombilla está quemada? (s/n)").toLowerCase
-            if(respuesta3 == "s"){
-                alert("Cambia la bombilla")
-            } else{
-                alert("Comprate una nueva lámpara")
-            }
-        }else{
+    let respuesta = prompt("¿La lampara funciona? (si/no)");//Variable para que el usuario introduzca el prompt
+    
+    
+    //condicional, si la respuesta a la primera pregunta es no, entra en el segundo if, si es que sí, pasa al ultimo else
+    if (respuesta.toLowerCase() == "no"){
+        let respuesta2 = prompt("Está enchufada? (si/no)");
+        let respuestaMin = respuesta2.toLowerCase();
+        //si la respuesta a la segunda pregunta es no nos muestra el alert, si es si, pasa al tercer if/else que está dentro del primer else
+        if(respuestaMin == "no"){
             alert("🤦‍♂️Pues enchúfala ")
-        };
-    };
-};
-lampara();
+           
+        }else{
+            let respuesta3 = prompt("La bombilla está quemada? (si/no)").toLowerCase();
+            if(respuesta3 == "no"){
+                alert("Comprate una nueva lámpara");
+               
+            } else{
+                alert("Cambia la bombilla");
+            }
+        }
+    }else {
+        alert("Qué bien!!");
+       
+    }
+}
+lampara()
+//Creamos 3 formas diferentes con toLowerCase, las 3 igual de válidas
